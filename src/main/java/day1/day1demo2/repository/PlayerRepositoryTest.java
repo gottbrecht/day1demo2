c8875d20bf34fb869721f13874010bafa585e6f8
gottbrecht <simo79j5@stud.kea.dk>
2023-08-24 10:07:13 +0200

    day1

---
+++ src/main/java/day1/day1demo2/repository/PlayerRepositoryTest.java
@@ -0,0 +1,12 @@
+package day1.day1demo2.repository;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+
+//@Autowired
+//PlayerRepository
+@Data
+
+public class PlayerRepositoryTest {
+
+}
